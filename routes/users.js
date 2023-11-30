ac5b64c1086f3c926dfd778a9fcbfc07ead3728e
okiramadani15 <okiramadani53@gmail.com>
2023-11-30 10:36:43 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,50 @@
 var express = require('express');
 var router = express.Router();
 const models = require('../models')
+const { Op } = require("sequelize")
+const path = require("path")
 
-router.get('/', async function(req, res, next) {
+
+router.get('/:id', async function(req, res, next) {
   try{
-  const users = await models.User.findAll();
+  const id = req.params.id
+  const users = await models.User.findByPk(id);
+  if(! users) {
+    return res.status(404).json({message: 'user not found'})
+  }
   res.json(users )
 }catch(err){
   console.log('ini error',err)
-  res.json(err)
+  res.status(500).json(err)
 }
+});
+
+router.get('/', async function (req, res, next) {
+  try {
+    const { page = 1, limit = 10, keyword = "", sort = "ASC" } = req.query
+    const { count, rows } = await models.User.findAndCountAll({
+      where: {
+        [Op.or]: [
+          { name: { [Op.iLike]: `%${keyword}%` } },
+          { phone: { [Op.iLike]: `%${keyword}%` } }
+        ]
+      },
+      order: [["name", sort]],
+      limit,
+      offset: (page - 1) * limit
+    });
+    const pages = Math.ceil(count / limit)
+    res.status(200).json({
+      phonebooks: rows,
+      page: Number(page),
+      limit: Number(limit),
+      pages: Number(pages),
+      total: count
+    })
+  } catch (err) {
+    console.log('ini erorr ', err)
+    res.status(500).json({ err })
+  }
 });
 
 router.post('/', async function(req, res, next) {
